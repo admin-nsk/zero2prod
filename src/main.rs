41369264a0561336ee63d664367d68ae82795820
Alexey Petrov <adm_sib@mail.ru>
2023-01-13 11:02:19 +0700

    chapter_4

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 use sqlx::postgres::PgPool;
 use std::net::TcpListener;
+
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
-#[tokio::main]
+
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set
+    let subscriber = get_subscriber(
+        "zero2prod".into(),
+        "info".into(),
+        || std::io::stdout()
+    );
+    init_subscriber(subscriber);
+
+    //init configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
-    // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
-    // reasons. This binding may cause issues in some environments. For example,
-    // it causes connectivity issues running in WSL2, where you cannot reach the
-    // server when it is bound to WSL2's localhost interface. As a workaround,
-    // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
-    // of the security implications when you expose the server on all interfaces.
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    println!("{}", address);
+    let address = format!(
+        "127.0.0.1:{}",
+        configuration.application_port
+    );
+    println!("Finance service running {}", address);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
